8dc330a6650f647db197106c62c3a416818b3ea1
tanayvaswani <vaswani.tanay9@gmail.com>
2024-04-17 22:37:45 +0530

    request: BHAI PLEASE SILLY ERRORS KARNA CHODH DE AUR GLOBALS.CSS KO THODI SI IZZAT DEDE, THANKS

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,15 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient;
+const client = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  // should add zod validation here
+
   const user = await client.user.create({
     data: {
       username: body.username,
-      password: body.password
+      password: body.password,
     },
   });
 
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,8 +13,8 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -32,54 +32,56 @@ export default function ProfileForm() {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-
     try {
       await fetch("/api/signup", {
         method: "POST",
         body: JSON.stringify(values),
-      });
-
-      // TODO: close dialog after submission
+      })
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
     } catch (error) {
       console.log("client side error", error);
     }
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-             
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>password</FormLabel>
-              <FormControl>
-                <Input placeholder="password" {...field} />
-              </FormControl>
-      
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-  
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
-};
+    <div className="flex items-center justify-center h-full">
+      <div className="min-h-44 w-80 p-6 bg-neutral-900 border rounded-md shadow-lg">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
